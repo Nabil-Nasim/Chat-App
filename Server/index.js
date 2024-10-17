602aa0ac198b7500dc460813168325704022abe0
Nabil Nasim <nabilnasim000@gmail.com>
2024-10-18 01:42:34 +0500

    Server Setup and Mongodb Setup

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL
+
+//Used for communication between servers
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}))
+
+//To get cookies form the frontend.Acts as a middleware here
+app.use(cookieParser())
+app.use(express.json())
+
+const server = app.listen(port, () => {
+    console.log(`Server is running at https://localhost:${port}`)
+})
+mongoose.connect(databaseURL).then(() => {
+    console.log("Database Connected Successfully")
+}).catch(err => console.log(err))
