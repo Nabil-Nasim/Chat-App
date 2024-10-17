d73eb33ecba2feb072f13867136bff1ccfce21da
Nabil Nasim <nabilnasim000@gmail.com>
2024-10-18 03:13:16 +0500

    User Models created and Handled signup

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-
+import authRoutes from "./routes/AuthRoutes.js";
 dotenv.config()
 
 const app = express()
@@ -20,6 +20,8 @@ app.use(cors({
 //To get cookies form the frontend.Acts as a middleware here
 app.use(cookieParser())
 app.use(express.json())
+
+app.use("/api/auth", authRoutes)
 
 const server = app.listen(port, () => {
     console.log(`Server is running at https://localhost:${port}`)
---
+++ Server/models/UserModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import {
+    genSalt
+} from "bcrypt"
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Passowrd is required"],
+    },
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    color: {
+        type: Number,
+        required: false,
+    },
+    profileSetup: {
+        type: Boolean,
+        default: false
+    }
+})
+UserSchema.pre("save", async function (next) {
+    const salt = await genSalt()
+    this.password = await hash(this.password, salt)
+    next()
+})
+
+const User = mongoose.model("Users", UserSchema)
+export default User;
