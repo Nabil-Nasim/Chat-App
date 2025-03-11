42cda0dbe07459241bf7daffe63165825034ff25
Nabil-Nasim <nabilnasim000@gmail.com>
2025-03-12 00:55:55 +0500

    Get Channel Messages Api Done

--- Server/routes/ChannelRoutes.js
+++ Server/routes/ChannelRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import {createChannel, getUserChannel} from "../controllers/ChannelController.js";
+import {createChannel, getUserChannel,getChannelMessages} from "../controllers/ChannelController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const channelRoutes = Router();
 
 channelRoutes.post("/create-channel", verifyToken, createChannel);
 channelRoutes.get("/get-user-channel", verifyToken, getUserChannel);
+channelRoutes.get("/get-channel-messages/:channelId", verifyToken, getChannelMessages);
 export default channelRoutes;
