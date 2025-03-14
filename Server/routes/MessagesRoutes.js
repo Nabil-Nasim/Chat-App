72d8f12511b0fa4e3a2af5a941722600d15f14e0
Nabil Nasim <113380699+Nabil-Nasim@users.noreply.github.com>
2025-03-15 01:07:58 +0500

    Update MessagesRoutes.js

--- Server/routes/MessagesRoutes.js
+++ Server/routes/MessagesRoutes.js
@@ -4,7 +4,10 @@ import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import multer from "multer"
 
 const messagesRoutes = Router();
-const upload = multer({ dest: 'uploads/files' })
+// const upload = multer({ dest: 'uploads/files' })
+const upload = multer({
+    dest: "/tmp/uploads/files"  // Use /tmp for file uploads
+});
 messagesRoutes.post("/get-messages",verifyToken, getMessages);
 messagesRoutes.post("/upload-file",verifyToken, upload.single("file"), uploadFile);
 
