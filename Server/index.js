42617e4a1a0356303d107f21f6ca3eeb4a2e4408
Nabil Nasim <113380699+Nabil-Nasim@users.noreply.github.com>
2025-03-15 02:15:19 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,8 @@ const databaseURL = process.env.DATABASE_URL
 app.use(cors({
     origin: process.env.ORIGIN || "https://chat-app-frontend-omega-wine.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true
+    credentials: true,
+    allowedHeaders: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
 }))
 
 
