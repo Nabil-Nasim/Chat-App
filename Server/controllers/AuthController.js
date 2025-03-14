e46e8f59ea40793a4b5ccf0b6bf9ade24dabdcb0
Nabil Nasim <113380699+Nabil-Nasim@users.noreply.github.com>
2025-03-15 01:14:35 +0500

    Update AuthController.js

--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -169,7 +169,8 @@ export const addProfileImage = async (req, res, next) => {
             return res.status(400).send("File is required")
         }
         const date = Date.now();
-        let fileName = `/tmp/uploads/profiles/${date}${req.file.originalname}`;
+        // let fileName = "uploads/profiles/" + date + req.file.originalname ( For Local Host)
+        let fileName = `/tmp/uploads/profiles/${date}${req.file.originalname}`; //For Vercel Deployment
         renameSync(req.file.path, fileName)
 
         const updatedUser = await User.findByIdAndUpdate(req.userId, {
