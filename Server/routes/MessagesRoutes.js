34d526cdc14e769456644fe9647fc3200f520c6a
Nabil Nasim <113380699+Nabil-Nasim@users.noreply.github.com>
2025-03-15 01:16:59 +0500

    Update MessagesRoutes.js

--- Server/routes/MessagesRoutes.js
+++ Server/routes/MessagesRoutes.js
@@ -4,9 +4,9 @@ import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import multer from "multer"
 
 const messagesRoutes = Router();
-// const upload = multer({ dest: 'uploads/files' })
+// const upload = multer({ dest: 'uploads/files' }) ( For Local Host)
 const upload = multer({
-    dest: "/tmp/uploads/files"  // Use /tmp for file uploads
+    dest: "/tmp/uploads/files"  //For Vercel Deployment
 });
 messagesRoutes.post("/get-messages",verifyToken, getMessages);
 messagesRoutes.post("/upload-file",verifyToken, upload.single("file"), uploadFile);
